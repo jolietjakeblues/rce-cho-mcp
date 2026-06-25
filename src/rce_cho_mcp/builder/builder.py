b6896df97220cb39f1f45ebdbac711c8b0062939
jolietjakeblues <jolietjakeblues64@gmail.com>
2026-06-25 10:59:24 +0200

    fix: builder

--- src/rce_cho_mcp/builder/builder.py
+++ src/rce_cho_mcp/builder/builder.py
@@ -18,17 +18,17 @@ PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 PREFIX rn: <https://data.cultureelerfgoed.nl/term/id/rn/2/>
 
 SELECT (COUNT(DISTINCT ?rm) AS ?aantal)
-WHERE {
-  GRAPH graph:instanties-rce {
+WHERE {{
+  GRAPH graph:instanties-rce {{
     ?rm a ceo:Rijksmonument .
     ?rm ceo:heeftJuridischeStatus rn:b2d9a59a-fe1e-4552-9a05-3c2acddff864 .
     ?rm ceo:heeftBasisregistratieRelatie ?rel .
     ?rel ceo:heeftGemeente ?gemeente .
-  }
+  }}
 
-  GRAPH graph:owms {
-    ?gemeente skos:prefLabel "Zeist"@nl .
-  }
-}
+  GRAPH graph:owms {{
+    ?gemeente skos:prefLabel "{gemeente}"@nl .
+  }}
+}}"""
 
     raise ValueError(f"Unsupported query plan: {plan}")
